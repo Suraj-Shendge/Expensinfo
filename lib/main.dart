ed573dcec8ff7271706a0ebf30d6b640524389a7
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-02-26 15:14:26 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-
 import 'core/theme.dart';
 import 'features/dashboard/dashboard_screen.dart';
-import 'data/database_service.dart';
-import 'data/models/expense_model.dart';
 
-const MethodChannel _channel = MethodChannel("background_channel");
-
-Future<void> main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // 🔥 Background notification action handler
-  _channel.setMethodCallHandler((call) async {
-    if (call.method == "insertExpense") {
-      final data = Map<String, dynamic>.from(call.arguments);
-
-      await DatabaseService.instance.insertExpense(
-        Expense(
-          amount: (data["amount"] as num).toDouble(),
-          merchant: data["merchant"] ?? "",
-          category: data["category"] ?? "Other",
-          date: DateTime.now(),
-        ),
-      );
-    }
-  });
-
   runApp(const ExpensinfoApp());
 }
 
