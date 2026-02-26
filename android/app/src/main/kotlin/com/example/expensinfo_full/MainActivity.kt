745600ce0c185bb7f3460a967bc5aac0337da509
Suraj-Shendge <124484652+Suraj-Shendge@users.noreply.github.com>
2026-02-26 12:57:01 +0530

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/expensinfo_full/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/expensinfo_full/MainActivity.kt
@@ -1,5 +1,28 @@
-package com.example.expensinfo_full
+package com.your.package.name
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.content.IntentFilter
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "sms_channel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        val receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                val smsBody = intent?.getStringExtra("sms_body")
+                MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+                    .invokeMethod("onSmsReceived", smsBody)
+            }
+        }
+
+        registerReceiver(receiver, IntentFilter("SMS_RECEIVED_INTERNAL"))
+    }
+}
